0648090a030e63697aaf8919c10936c120857166
pankajyadav22-26 <142982981+pankajyadav22-26@users.noreply.github.com>
2024-08-09 20:38:16 +0530

    learn

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 //placing order 
 const placeOrder = async (req, res) => {
-    const frontend_url = "hhttp://localhost:5173"
+    const frontend_url = "https://foodwebsite-frontend.onrender.com"
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
